e15c8f77b1699eb11196dbf61196481c021c4390
Daniel Collis-Puro <dan@collispuro.net>
2010-04-20 13:31:25 -0400

    Add the ability to limit the number of items in a feed

--- app/portlets/feed_portlet.rb
+++ app/portlets/feed_portlet.rb
@@ -4,6 +4,12 @@ class FeedPortlet < Portlet
   def render
     raise ArgumentError, "No feed URL specified" if self.url.blank?
     @feed = Feed.find_or_create_by_url(self.url).parsed_contents
+    if @portlet.limit.to_i != 0
+      @items = @feed.items[0..(@portlet.limit.to_i - 1)]
+    else
+      @items = @feed.items
+    end
     instance_eval(self.code) unless self.code.blank?
   end
+
 end
