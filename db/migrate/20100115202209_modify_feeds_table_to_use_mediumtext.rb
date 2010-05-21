27a1d4f6591b73521fa8f0227fdeb1eb01fadf11
Daniel Collis-Puro <djcp@dv2120us.(none)>
2010-05-20 22:15:44 -0400

    Fix this migration to only run under mysql, as it's unecessary and broken under postgres.

--- db/migrate/20100115202209_modify_feeds_table_to_use_mediumtext.rb
+++ db/migrate/20100115202209_modify_feeds_table_to_use_mediumtext.rb
@@ -1,9 +1,15 @@
 class ModifyFeedsTableToUseMediumtext < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE feeds MODIFY COLUMN contents MEDIUMTEXT"
+    if Feed.connection.adapter_name == 'MySQL'
+      #The postgres text column suffices, and this syntax and column type don't work in pg.
+      execute "ALTER TABLE feeds MODIFY COLUMN contents MEDIUMTEXT"
+    end
   end
 
   def self.down
-    execute "ALTER TABLE feeds MODIFY COLUMN contents TEXT"
+    if Feed.connection.adapter_name == 'MySQL'
+      #The postgres text column suffices, and this syntax and column type don't work in pg.
+      execute "ALTER TABLE feeds MODIFY COLUMN contents TEXT"
+    end
   end
 end
